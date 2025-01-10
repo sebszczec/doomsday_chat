31ba6c3588e03b8e514722368917769959a24e48
Slaugh <sebszczec@wp.pl>
2025-01-10 11:22:37 +0100

    Simple file server (and test client) implemented

--- src/file_server.rs
+++ src/file_server.rs
@@ -1,5 +1,11 @@
 use crate::tcp_server::Connection;
 
+use log::info;
+use tokio::fs::File;
+use tokio::io::AsyncReadExt;
+use tokio::io::AsyncWriteExt;
+use std::time::Instant;
+
 #[derive(Clone)]
 pub struct FileConnection {
 
@@ -12,11 +18,35 @@ impl FileConnection {
 }
 
 impl Connection for FileConnection {
-    async fn handle(self, tcp: tokio::net::TcpStream) -> Result<bool, bool> {
+    async fn handle(self, mut tcp: tokio::net::TcpStream) -> Result<bool, bool> {
+        let mut file = File::open("test_input.jpg").await.unwrap();
+
+        let mut buffer = [0u8; 1024];
+
+        let now = Instant::now();
+        let mut file_size: f32 = 0.0;
+
+        loop {
+            let bytes_read = file.read(&mut buffer).await.unwrap();
+            if bytes_read == 0 {
+                break;
+            }
+
+            file_size += bytes_read as f32;
+            tcp.write_all(&buffer[..bytes_read]).await.unwrap();
+        }
+
+        let duration = now.elapsed();
+        let mbytes = file_size / 1024.0 / 1024.0;
+        let tput = mbytes / duration.as_secs_f32();
+
+        info!("File sent, size: {:.2} mB throughput: {:.2} mB/s", mbytes, tput);   
+
         Ok(true)
     }
 
     async fn setup_broadcast(self) -> Result<bool, bool> {
+        info!("No broadcast procedure");
         Ok(true)
     }
 
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 mod chat_server;
 use std::time::Duration;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpStream;
+use tokio::fs::File;
+use std::time::Instant;
+use log::info;
 
 use crate::chat_server::ChatConnection;
 mod file_server;
@@ -18,8 +23,33 @@ async fn main() {
     let file_server = TcpServer::new("192.168.0.123:9898", FileConnection::new()).await.unwrap();
     tokio::spawn(file_server.start_loop());
 
+    let mut connection = TcpStream::connect("192.168.0.123:9898").await.unwrap();
+    let mut file = File::create("test_output.jpg").await.unwrap();
+
+    let mut buffer = [0u8; 1024];
+
+    let now = Instant::now();
+    let mut file_size: f32 = 0.0;
+
+    loop {
+        let received = connection.read(&mut buffer[..]).await.unwrap();
+
+        if received == 0 {
+            break;
+        }
+
+        file_size += received as f32;
+
+        file.write_all(&buffer[..received]).await.unwrap();
+    }
+
+    let duration = now.elapsed();
+    let mbytes = file_size / 1024.0 / 1024.0;
+    let tput = mbytes / duration.as_secs_f32();
+
+    info!("File received, size: {:.2} mB throughput: {:.2} mB/s", mbytes, tput);   
+
     loop {
         tokio::time::sleep(Duration::from_millis(100)).await;
     }
 }
-
